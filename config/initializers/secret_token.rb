88d3c0adf8eb009bb8dffbbccb3bd52bcf44c039
lionel-rigoux <lionel.rigoux@gmail.com>
2013-08-03 18:30:02 +0200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blabla::Application.config.secret_key_base = '083b136b9b5ddda1782c0e10cfff622d6c7f6cc930d0ba4dbefe7aca5f4f2bf09761e5d7a9bb05893ce8b533249e97d6c7d652d17cde8311e5215b9020e815c8'
